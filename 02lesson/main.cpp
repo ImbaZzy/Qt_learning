10a38a6d43013ae22a2c1dbeabee7ddb11dd4a4d
ZhangZeyu <imbazzy159@163.com>
2018-12-04 13:12:56 +0800

    use Debug class and Lambda

--- 02lesson/main.cpp
+++ 02lesson/main.cpp
@@ -1,12 +1,15 @@
 // Qt5
 #include <qapplication.h>
 #include <qpushbutton.h>
+#include <qdebug.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc,argv);
     QPushButton button("Quit");
-    QObject::connect(&button, &QPushButton::clicked, &QApplication::quit);
+    QObject::connect(&button, &QPushButton::clicked, /*&QApplication::quit*/[](bool){
+        qDebug() << "You clicked me!";
+    });
     button.show();
     return a.exec();
 }
