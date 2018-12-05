a0658a0cab9b4f3a86b298d7be9303d9c226c399
ZhangZeyu <imbazzy159@163.com>
2018-12-05 08:56:03 +0800

    add notes

--- 03lesson/newpaper.h
+++ 03lesson/newpaper.h
@@ -5,18 +5,18 @@
 
 class Newspaper:public QObject
 {
-    Q_OBJECT
+    Q_OBJECT//必須要加上的宏
 public:
-    Newspaper(const QString & name):
+    Newspaper(const QString & name)://構造函數，用於初始化Newspaper類
         m_name(name){}
 
     void send()
     {
-        emit newPaper(m_name);
+        emit newPaper(m_name);//在適當的地方將信息emit出去
     }
 
 signals:
-    void newPaper(const QString & name);
+    void newPaper(const QString & name);//signails的函數返回值爲void
 
 private:
     QString m_name;
